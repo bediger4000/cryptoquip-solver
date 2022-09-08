78f387c0e5fc448cf61fc5c2b896d934addabcde
Bruce Ediger <bediger8@gmail.com>
2022-09-08 16:49:50 -0600

    add func NewRuneDict

--- qp/dict.go
+++ qp/dict.go
@@ -4,7 +4,20 @@ import (
 	"bufio"
 	"log"
 	"os"
+	"strings"
+
+	"golang.org/x/text/unicode/norm"
 )
+
+// Entry represents the letters of the words having a particular configuration
+// Enciphered word "zoyzkojvx" has configuration "012031456", shared with
+// 15 words in /usr/share/dict/words.
+type Entry struct {
+	Length int
+	Runes  map[int]map[rune]bool
+	// Runes[0] are all the dictionary words' first letters.
+	// the first letters are keys in the map-that-is-the-value
+}
 
 func NewDict(fileName string) (map[string][]string, error) {
 	fin, err := os.Open(fileName)
@@ -22,6 +35,8 @@ func NewDict(fileName string) (map[string][]string, error) {
 	for scanner.Scan() {
 		lineCounter++
 		line := scanner.Text()
+		line = strings.ToLower(line)
+		line = norm.NFC.String(line)
 
 		config := StringConfiguration(line)
 		d[config] = append(d[config], line)
@@ -32,4 +47,44 @@ func NewDict(fileName string) (map[string][]string, error) {
 	}
 
 	return d, nil
+}
+
+func NewRunesDict(wordDict map[string][]string) map[string]*Entry {
+	d := make(map[string]*Entry)
+
+	for configuration, words := range wordDict {
+		if entry, ok := d[configuration]; ok {
+			// encountered this configuration before
+			for _, word := range words {
+				for idx, r := range word {
+					if r > 'z' || r < 'a' {
+						continue
+					}
+					if found := entry.Runes[idx][r]; !found {
+						// haven't seen this letter at this position
+						entry.Runes[idx][r] = true
+					}
+				}
+			}
+		} else {
+			// new to us configuration
+			var e Entry
+			e.Length = len(configuration)
+			e.Runes = make(map[int]map[rune]bool)
+			for _, word := range words {
+				for idx, r := range word {
+					if r > 'z' || r < 'a' {
+						continue
+					}
+					if _, ok := e.Runes[idx]; !ok {
+						e.Runes[idx] = make(map[rune]bool)
+					}
+					e.Runes[idx][r] = true
+				}
+			}
+			d[configuration] = &e
+		}
+	}
+
+	return d
 }
