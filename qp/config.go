d84a9fd7ef3f7d0b87b659c96f8b7f456518d4bd
Bruce Ediger <bediger8@gmail.com>
2024-03-11 21:11:48 -0600

    change definition of word "shape"
    "Shape" or "configuration" of a word now includes
    ASCII/UTF-8 single quotes.

--- qp/config.go
+++ qp/config.go
@@ -1,49 +1,42 @@
 package qp
 
-var scorecard [124]rune
+import "unicode"
 
 func StringConfiguration(line string) string {
-	key := make([]rune, 0, len(line))
-
-	idx := 0
-
-	ln := len(line)
-	if ln >= 3 && line[ln-1] == 'l' && line[ln-2] == 'l' && line[ln-3] == '\'' {
-		line = line[:ln-3]
-		ln = len(line)
-	}
-	if ln >= 2 && line[ln-1] == 's' && line[ln-2] == '\'' {
-		line = line[:ln-2]
-		ln = len(line)
-	}
-	if line[ln-1] == '\'' {
-		line = line[:ln-1]
-	}
 
 	if len(line) == 0 {
 		return ""
 	}
 
-	for i, r := range line {
-		if i == 0 {
-			key = append(key, '0')
-			scorecard[idx] = r
+	key := make([]rune, len(line))
+	idx := 0
+
+	var scorecard [255]rune
+
+	i := 0
+
+	for _, r := range line {
+		l := unicode.ToLower(r)
+		if unicode.IsLetter(l) {
+			if previous := scorecard[int(l)]; previous != 0 {
+				key[idx] = previous
+			} else {
+				// single-quote appears before '0' in Unicode,
+				// so only single-quotes that appear in input words
+				// appear in "shape" of those words
+				scorecard[l] = '0' + rune(i)
+				i++
+				key[idx] = scorecard[l]
+			}
 			idx++
 			continue
 		}
-		foundit := false
-		for j := 0; j < idx; j++ {
-			if scorecard[j] == r {
-				key = append(key, rune('0'+j))
-				foundit = true
-				break
-			}
-		}
-		if !foundit {
-			scorecard[idx] = r
-			key = append(key, rune('0'+idx))
+		if l == '\'' {
+			key[idx] = l
 			idx++
+			continue
 		}
 	}
+
 	return string(key)
 }
